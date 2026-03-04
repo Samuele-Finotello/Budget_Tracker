6c51d77f6aa6147f3b0edc48e3b715953040724e
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-03-04 20:31:34 +0100

    [add] feat sections budget

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,17 @@ export default function App() {
   return (
     <div className="max-w-6xl mx-auto my-10">
       <h1 className="text-4xl font-bold text-slate-900 text-center my-10">Budget Tracker</h1>
+      <div className="flex justify-around items-center">
+        <div className="border-2 border-gray-200 rounded-3xl p-4 shadow-sm w-72">
+          <p className="text-sm text-slate-500 font-medium text-center mb-1">Budget Totale</p>
+        </div>
+        <div className="border-2 border-gray-200 rounded-3xl p-4 shadow-sm w-72">
+          <p className="text-sm text-slate-500 font-medium text-center mb-1">Budget Speso</p>
+        </div>
+        <div className="border-2 border-gray-200 rounded-3xl p-4 shadow-sm w-72">
+          <p className="text-sm text-slate-500 font-medium text-center mb-1">Budget Residuo</p>
+        </div>
+      </div>
     </div>
   )
 }
