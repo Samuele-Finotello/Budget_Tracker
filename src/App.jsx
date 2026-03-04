f0996fbc7b2ef37a85c946ee694b8a1dfe997717
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-03-04 20:15:55 +0100

    [add] feat localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import { useState, useEffect } from "react"
 
 export default function App() {
 
-  return (
-    <>
+  const [list, setList] = useState(() => {
+    const saved = localStorage.getItem('list');
+    return saved ? JSON.parse(saved) : [];
+  })
 
-    </>
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list])
+
+  return (
+    <div className="max-w-6xl mx-auto my-10">
+      <h1 className="text-4xl font-bold text-slate-900 text-center my-10">Budget Tracker</h1>
+    </div>
   )
 }
