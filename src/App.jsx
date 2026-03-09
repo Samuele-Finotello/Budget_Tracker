7aedc68f7c1e2b2496e18950cda44a5de5ed98a0
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-03-09 12:35:15 +0100

    [add] feat create operation card

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,32 @@ export default function App() {
       {list.length === 0 ?
         <p className="text-2xl font-bold text-slate-900 text-center mt-18">📭 Nessuna operazione trovata. Inizia aggiungendone una sopra!</p> :
         <div className="my-5">
-
+          {list.map(operation => {
+            return (
+              <div key={operation.id}>
+                <div className={`bg-white p-6 rounded-2xl shadow-sm hover:shadow-lg transition-all duration-300 border border-slate-100 flex justify-between items-center gap-4 mb-3 ${operation.import > 0 ?
+                  'hover:bg-green-50' :
+                  'hover:bg-red-50'}`}>
+                  <div className="flex items-center gap-4">
+                    <div className={`p-3 rounded-full ${operation.import > 0 ?
+                      'bg-emerald-100 text-emerald-600' :
+                      'bg-red-100 text-red-600'}`}>
+                      <div className="flex items-center justify-center w-6 h-6">
+                        <span className={`text-2xl font-bold ${operation.import > 0 ?
+                          'text-green-500' :
+                          'text-red-500'}`}
+                        >€</span>
+                      </div>
+                    </div>
+                    <div>
+                      <p className="text-md text-slate-500 font-medium">{operation.description}</p>
+                      <p className="text-2xl font-bold text-slate-900">{formattedBudget(operation.import)}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
         </div>
       }
     </div>
