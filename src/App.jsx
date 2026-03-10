8a35b5b655a3b92cba38a3e9aaa1f11278ee1680
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-03-10 14:06:48 +0100

    [add] feat create modal

--- src/App.jsx
+++ src/App.jsx
@@ -160,6 +160,12 @@ export default function App() {
           })}
         </div>
       }
+      {modal && <div className={`fixed bottom-5 right-5 flex items-center gap-3 px-6 py-4 rounded-2xl shadow-2xl ${message === 'Importo non valido' || message === 'Descrizione non valida' ? 'bg-red-200' : 'bg-green-200'}`}>
+        <div>
+          <button className="cursor-pointer" onClick={() => setModal(false)}>✕</button>
+        </div>
+        <p className="text-slate-900">{message}</p>
+      </div>}
     </div>
   )
 }
