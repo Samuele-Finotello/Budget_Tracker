0e9cfef40394ae752aa3f63bcfc98071421fba24
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-03-17 14:25:25 +0100

    [add] feat create component FormCard

---
+++ src/components/FormCard.jsx
@@ -0,0 +1,28 @@
+export default function FormCard({ addOperation, selectionRef, descriptionRef, importRef }) {
+
+  return (
+    <form onSubmit={addOperation} className="my-10">
+      <p className="text-md font-medium text-slate-700 mb-1">Inserisci un'operazione</p>
+      <div className="bg-slate-100 p-6 rounded-3xl flex gap-4 items-end">
+        <div>
+          <label className="block text-sm font-medium text-slate-700 mb-1">Seleziona</label>
+          <select name="selection" ref={selectionRef} className="bg-slate-900 text-white px-6 py-2 rounded-xl font-bold">
+            <option value={'entrata'}>Entrata</option>
+            <option value={'uscita'}>Uscita</option>
+          </select>
+        </div>
+        <div className="flex-1">
+          <label className="block text-sm font-medium text-slate-700 mb-1">Descrizione</label>
+          <input type="text" name="description" ref={descriptionRef} className="w-full p-2 rounded-xl border-none ring-1 ring-slate-300 focus:ring-2 focus:ring-blue-500" placeholder="Es. Affitto" />
+        </div>
+        <div className="w-40">
+          <label className="block text-sm font-medium text-slate-700 mb-1">Importo (€)</label>
+          <input type="number" name="import" step={0.01} ref={importRef} className="w-full p-2 rounded-xl border-none ring-1 ring-slate-300 focus:ring-2 focus:ring-blue-500 outline-none" />
+        </div>
+        <button type="submit" className="bg-slate-900 text-white px-6 py-2 rounded-xl font-bold">
+          Aggiungi
+        </button>
+      </div>
+    </form>
+  )
+}
