58cc32b8c9bdb8a30f309446ce0ae7be44161c98
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-03-22 14:25:49 +0100

    [add] feat create component ModalConfirmAll

---
+++ src/components/ModalConfirmAll.jsx
@@ -0,0 +1,18 @@
+export default function ModalConfirmAll({ setModalConfirmAll, confirmDeleteAll }) {
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+      <div className="absolute inset-0 bg-slate-900/60 backdrop-blur-sm flex items-center justify-center">
+        <div className="relative bg-white p-8 rounded-3xl shadow-2xl max-w-md">
+          <div>
+            <h2 className="text-xl font-bold text-slate-900 mb-7">Confermi di voler eliminare tutte le operazioni?</h2>
+            <div className="flex justify-between">
+              <div><button className="bg-slate-900 text-white px-6 py-2 rounded-xl font-bold" onClick={() => setModalConfirmAll(false)}>Annulla</button></div>
+              <div><button className="bg-red-800 text-white px-6 py-2 rounded-xl font-bold" onClick={() => confirmDeleteAll()}>Conferma</button></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
